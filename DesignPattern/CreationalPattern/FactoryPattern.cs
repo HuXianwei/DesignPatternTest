8dda2d2b547946840c70f2f83c8b5908ddf0d1ad
XIANWEI HU <820693371@qq.com>
2017-10-30 22:11:12 +0800

    添加项目

---
+++ DesignPattern/CreationalPattern/FactoryPattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.CreationalPattern
+{
+    /// <summary>
+    /// 工厂模式
+    /// </summary>
+    internal class FactoryPattern
+    {
+        public interface Shape
+        {
+
+        }
+        
+    }
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -2,6 +2,9 @@
 
 namespace DesignPattern
 {
+    /// <summary>
+    /// 设计模式测试类
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
